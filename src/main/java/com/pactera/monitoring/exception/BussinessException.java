52bd13089ad16ff551c6991cee6ec8845afa6155
844836976@qq.com <844836976@qq.com>
2020-04-07 09:57:37 +0800

    自定义异常

---
+++ src/main/java/com/pactera/monitoring/exception/BussinessException.java
@@ -0,0 +1,16 @@
+package com.pactera.monitoring.exception;
+
+/**
+ * 业务异常
+ */
+public class BussinessException extends Exception {
+
+    public BussinessException() {
+        super();
+    }
+
+
+    public BussinessException(String message) {
+        super(message);
+    }
+}
